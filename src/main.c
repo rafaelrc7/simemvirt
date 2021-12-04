2789d60483f0f724d5f5ca4db64f5351317ba394
rafaelrc7 <rafaelrc7@gmail.com>
2021-12-03 21:03:26 -0300

    CLI arguments parsing

--- src/main.c
+++ src/main.c
@@ -1,9 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+int argtoui(const char *arg, unsigned int *num);
+
+int main(int argc, char **argv)
 {
-	puts("Hello, world");
+	unsigned int page_size, mem_size;
+	const char *alg_name;
+	const char *file_name;
+
+	if (argc != 5) {
+		fprintf(stderr, "USAGE: %s <algoritimo> <arquivo de enderecos>"
+						" <tamanho de cada pagina> <tamanho da memoria>\n",
+						argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	alg_name = argv[1];
+	file_name = argv[2];
+	if (!argtoui(argv[3], &page_size)) {
+		fprintf(stderr, "Third argument is invalid.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (!argtoui(argv[4], &mem_size)) {
+		fprintf(stderr, "Fourth argument is invalid.\n");
+		exit(EXIT_FAILURE);
+	}
+
 
 	return 0;
+}
+
+int argtoui(const char *arg, unsigned int *num)
+{
+	char *endptr;
+
+	if (*arg == 0)
+		return 0;
+
+	*num = (unsigned int)strtoul(arg, &endptr, 10);
+	if (*endptr != 0)
+		return 0;
+
+	return 1;
 }
 
