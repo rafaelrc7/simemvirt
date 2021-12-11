df7b8f6259f0f80ec33ed50647540b07d9c27a43
pedr0piquet <pedro.piquet11@gmail.com>
2021-12-10 22:28:19 -0300

    AGORA FOI

--- src/lfu.c
+++ src/lfu.c
@@ -7,30 +7,22 @@
 #include "frame_stack.h"
 #include "lfu.h"
 
-int entrou = 0, entrou1 = 0, carregado = 0;
 
 void lfu(uint32_t page_id, Memory_page *page_table, Memory_frame *physical_mem, size_t num_mem_frames, Free_frame **free_frame_stack){
 
-	//uint32_t pag;
 	unsigned long int i = 0;
-	//int entrou, entrou1;
 	if(!page_table[page_id].is_loaded){
-		entrou++;
-		//printf("entrou no if %d\n", entrou);
 		if(*free_frame_stack){ //stack ainda tem espaço
 			swapin(physical_mem, free_frame_stack, page_table, page_id); //carrega
 			physical_mem[page_table[page_id].addr].A ++;
-			//pag = physical_mem[c1_addr].page_id;
 		}
 		else{
 			//procurar frame menos utilizado
-			entrou1++;
-			//printf("entrou no else %d\n", entrou1);
 			 unsigned long int menor = 1;
 			 int id_temp = 0;
 			for (i = 0; i < num_mem_frames; ++i){
 
-				if(page_table[i].is_loaded && (physical_mem[i].A < menor)){//frame esta carregado e M < menor
+				if(page_table[i].is_loaded && (physical_mem[i].A < menor)){//frame esta carregado e A < menor
 					menor = (int) physical_mem[i].A;
 					id_temp = i;
 				}
@@ -45,9 +37,6 @@ void lfu(uint32_t page_id, Memory_page *page_table, Memory_frame *physical_mem, 
 		}
 	}
 	else{
-		//pag = physical_mem[c1_addr].page_id;
-		carregado++;
-		//printf("estava carregado %d\n", carregado);
 		physical_mem[page_table[page_id].addr].A ++; //apenas adiciona na frequencia
 	}
 }
